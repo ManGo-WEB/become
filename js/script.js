3a75e924a8e7d501d6751f64fc8c82185b47e856
Nikita Vampilov <info@mango-web.ru>
2024-05-25 12:20:15 +1000

    Add Hero block

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 "use strict";
-
+// Award close icon
 const award = document.querySelector(".award__img");
 
 award.addEventListener("click", () => {
-	document.querySelector(".award__icon").remove();
+	document.querySelector(".award").remove();
+});
+
+// Play video
+const btnClickVideo = document.getElementById("play-video");
+
+const video = document.getElementById("active-video");
+function openVideo() {
+	video.play();
+}
+
+btnClickVideo.addEventListener("click", () => {
+	openVideo();
 });
